afb4299e1ae98daa0f80318e263478972c02062d
David Tan <davidtandev@gmail.com>
2021-10-14 09:51:33 +0800

    create new private aks cluster with rbac enabled (default)

---
+++ notes/aks-create.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+az aks create \
+    --resource-group teamResources \
+    --name team5RBACCluster \
+    --network-plugin azure \
+    --vnet-subnet-id /subscriptions/03271573-4285-43f4-8557-808369b280af/resourceGroups/teamResources/providers/Microsoft.Network/virtualNetworks/vnet/subnets/vm-subnet \
+    --docker-bridge-address 172.17.0.1/16 \
+    --dns-service-ip 10.3.0.10 \
+    --service-cidr 10.3.0.0/24 \
+    --generate-ssh-keys \
+    --enable-private-cluster \
+    --attach-acr /subscriptions/03271573-4285-43f4-8557-808369b280af/resourceGroups/teamResources/providers/Microsoft.ContainerRegistry/registries/registryznl4647
